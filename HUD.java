27803ac0afd5bad833c8abea6af32ea7801009c8
student <student@Stev1034-08.local>
2019-12-05 15:54:46 -0800

    added functionality for restart level button

--- HUD.java
+++ HUD.java
@@ -22,8 +22,40 @@ public class HUD extends JPanel implements MouseListener, KeyListener {
         D.hud = this;
         data.hud.setBackground(Color.RED);
         addMouseListener(this);
+        restartButton = new JButton("Restart Level");
+        this.add(restartButton);
+
+        restartButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //one of these conditions could be removed, but this will make certain no out of bounds issues happen
+                for(int i = 0; i < data.startPoints.size() && i < data.blocks.size(); i++){
+                    //move block and set back in play
+                    data.blocks.get(i).moveTo(data.startPoints.get(i).x,data.startPoints.get(i).y);
+                    data.blocks.get(i).onBoard = true;
+                    data.blocks.get(i).placed  = false;
+
+                    //reset holes to accept shapes again
+                    data.holes.get(i).filledWith = null;
+                    data.holes.get(i).filled     = false;
+                    
+                }
+                data.game.repaint();
+            }
+        }
+
+        );
+
 
     }
+
+
+
+
+
+
+
+
 
 
 }
